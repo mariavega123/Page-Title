51d4eb5da6a97edaf944435b8628dbf623b12711
mariavega123 <44841540+mariavega123@users.noreply.github.com>
2019-02-11 10:03:28 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".button1").dblclick(function(){
+    $(".img1").toggle();
+});
+
+$(".button2").click(function(){
+    $(".img2").fadeOut();
+});
+$(".button3").mouseenter(function(){
+    $(".img3").fadeOut();
+});
+$(".button4").mouseleave(function(){
+    $(".img4").fadeOut();
+});
+$(".button5").dblclick(function(){
+    $(".img5").slideToggle();
+});
+
+
+ 
+ 
+
